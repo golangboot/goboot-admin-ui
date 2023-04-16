8902949efd90b4418e2d5c9a5e058d9f8aa66971
鸿宇 <1527200768@qq.com>
2023-04-16 10:38:13 +0800

    v0.13.0 Initial commit

--- src/api/model/system.js
+++ src/api/model/system.js
@@ -180,44 +180,44 @@ export default {
 			}
 		},
 	},
-	dept: {
+	organization: {
 		tree: {
-			url: `${config.API_URL}/system/department/tree`,
+			url: `${config.API_URL}/system/organization/tree`,
 			name: "列表树",
 			get: async function(data){
 				return await http.get(this.url, data);
 			}
 		},
 		list: {
-			url: `${config.API_URL}/system/department`,
+			url: `${config.API_URL}/system/organization`,
 			name: "列表",
 			get: async function(data){
 				return await http.get(this.url, data);
 			}
 		},
 		add: {
-			url: `${config.API_URL}/system/department`,
+			url: `${config.API_URL}/system/organization`,
 			name: "添加",
 			post: async function(data){
 				return await http.post(this.url, data);
 			}
 		},
 		show: {
-			url: `${config.API_URL}/system/department`,
+			url: `${config.API_URL}/system/organization`,
 			name: "查看",
 			get: async function(data){
 				return await http.get(`${this.url}/${data.id}`, data);
 			}
 		},
 		update: {
-			url: `${config.API_URL}/system/department`,
+			url: `${config.API_URL}/system/organization`,
 			name: "更新",
 			put: async function(data){
 				return await http.put(`${this.url}`, data);
 			}
 		},
 		delete: {
-			url: `${config.API_URL}/system/department`,
+			url: `${config.API_URL}/system/organization`,
 			name: "删除",
 			delete: async function(data){
 				let id = data.id;
