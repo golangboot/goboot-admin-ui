27ae5533dbac34f633ff33e686bb67c0aaafe99f
鸿宇 <1527200768@qq.com>
2023-04-17 07:23:20 +0800

    v0.15.0 Initial commit

--- src/api/model/system.js
+++ src/api/model/system.js
@@ -228,6 +228,47 @@ export default {
 			}
 		},
 	},
+	permission: {
+		list: {
+			url: `${config.API_URL}/system/permission`,
+			name: "列表",
+			get: async function(data){
+				return await http.get(this.url, data);
+			}
+		},
+		add: {
+			url: `${config.API_URL}/system/permission`,
+			name: "添加",
+			post: async function(data){
+				return await http.post(this.url, data);
+			}
+		},
+		show: {
+			url: `${config.API_URL}/system/permission`,
+			name: "查看",
+			get: async function(data){
+				return await http.get(`${this.url}/${data.id}`, data);
+			}
+		},
+		update: {
+			url: `${config.API_URL}/system/permission`,
+			name: "更新",
+			put: async function(data){
+				return await http.put(`${this.url}`, data);
+			}
+		},
+		delete: {
+			url: `${config.API_URL}/system/permission`,
+			name: "删除",
+			delete: async function(data){
+				let id = data.id;
+				if (data.ids) {
+					id = data.ids.join(',');
+				}
+				return await http.delete(`${this.url}/${id}`, data);
+			}
+		},
+	},
 	dept: {
 		tree: {
 			url: `${config.API_URL}/system/department/tree`,
