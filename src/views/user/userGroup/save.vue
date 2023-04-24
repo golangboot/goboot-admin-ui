0f0542e46ab5c8fef31b9ff1629cd92a20e94f0f
鸿宇 <1527200768@qq.com>
2023-04-24 08:24:09 +0800

    v0.50.0 Initial commit

---
+++ src/views/user/userGroup/save.vue
@@ -0,0 +1,125 @@
+<template>
+	<el-dialog :title="titleMap[mode]" v-model="visible" :width="500" destroy-on-close @closed="$emit('closed')">
+		<el-form :model="form" :rules="rules" :disabled="mode=='show'" ref="dialogForm" label-width="100px" label-position="left">
+			<el-form-item label="用户组名称" prop="name">
+				<el-input v-model="form.name" clearable></el-input>
+			</el-form-item>
+			<el-form-item label="用户组编码" prop="code">
+				<el-input v-model="form.code" clearable></el-input>
+			</el-form-item>
+			<el-form-item label="所属部门" prop="parentId">
+				<el-cascader v-model="form.parentId" :options="departmentOptions" :props="departmentProps" :show-all-levels="false" :emitPath="false" placeholder="请选择部门" clearable></el-cascader>
+			</el-form-item>
+			<el-form-item label="排序" prop="sort">
+				<el-input-number v-model="form.sort" controls-position="right" :min="1" style="width: 100%;"></el-input-number>
+			</el-form-item>
+			<el-form-item label="是否有效" prop="status">
+				<el-switch v-model="form.status" :active-value="1" :inactive-value="0"></el-switch>
+			</el-form-item>
+			<el-form-item label="备注" prop="remark">
+				<el-input v-model="form.remark" clearable type="textarea"></el-input>
+			</el-form-item>
+		</el-form>
+		<template #footer>
+			<el-button @click="visible=false" >取 消</el-button>
+			<el-button v-if="mode!='show'" type="primary" :loading="isSaving" @click="submit()">保 存</el-button>
+		</template>
+	</el-dialog>
+</template>
+
+<script>
+	export default {
+		emits: ['success', 'closed'],
+		data() {
+			return {
+				mode: "add",
+				titleMap: {
+					add: '新增',
+					edit: '编辑',
+					show: '查看'
+				},
+				visible: false,
+				isSaving: false,
+				//表单数据
+				form: {
+					id:"",
+					name: "",
+					code: "",
+					label: "",
+					sort: null,
+					status: 1,
+					remark: ""
+				},
+				//验证规则
+				rules: {
+					name: [
+						{required: true, message: '请输入用户组名称'}
+					],
+				},
+				departmentOptions: [],
+				departmentProps: {
+					value: 'id',
+					label: 'name',
+					checkStrictly: true,
+					emitPath: false,
+				},
+			}
+		},
+		mounted() {
+			this.getDepartment()
+		},
+		methods: {
+			//显示
+			open(mode='add'){
+				this.mode = mode;
+				this.visible = true;
+				return this
+			},
+			async getDepartment(){
+				var res = await this.$API.system.dept.tree.get();
+				this.departmentOptions = res.data
+			},
+			//表单提交方法
+			submit(){
+				this.$refs.dialogForm.validate(async (valid) => {
+					if (valid) {
+						this.isSaving = true;
+						var res;
+						if (this.form.id) {
+							res = await this.$API.user.group.update.put(this.form)
+						} else {
+							res = await this.$API.user.group.add.post(this.form)
+						}
+						this.isSaving = false;
+						if(res.code == 200){
+							this.form = res.data
+							this.$emit('success', this.form, this.mode)
+							this.visible = false;
+							this.$message.success("操作成功")
+						}else{
+							this.$alert(res.message, "提示", {type: 'error'})
+						}
+					}
+				})
+			},
+			//表单注入数据
+			setData(data){
+				Object.assign(this.form, data)
+				if (data.id){
+					this.loading = true
+					const params = {
+						id: data.id
+					}
+					setTimeout(async ()=>{
+						var res = await this.$API.user.group.show.get(params)
+						this.loading = false
+						this.form = res.data
+					},100)
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
