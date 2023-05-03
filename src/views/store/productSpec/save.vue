d5b333c3c30498b8e519d395c12fc071a7e5dad0
鸿宇 <1527200768@qq.com>
2023-05-04 07:55:24 +0800

    v0.80.0 Initial commit

---
+++ src/views/store/productSpec/save.vue
@@ -0,0 +1,126 @@
+<template>
+	<el-dialog :title="titleMap[mode]" v-model="visible" destroy-on-close @closed="$emit('closed')">
+		<el-form :model="form" :rules="rules" :disabled="mode=='show'" ref="dialogForm" label-width="110px" label-position="left">
+			<el-form-item label="商品规格名称" prop="name">
+				<el-input v-model="form.name" clearable></el-input>
+			</el-form-item>
+			<el-form-item label="所属分类" prop="categoryIds">
+				<el-cascader v-model="form.categoryIds" :options="categoryOptions" :props="categoryProps" :show-all-levels="true" size="large" style="width:100%" placeholder="请选择所属分类" filterable clearable></el-cascader>
+			</el-form-item>
+			<el-form-item label="排序" prop="sort">
+				<el-input-number v-model="form.sort" controls-position="right" style="width: 100%;"></el-input-number>
+			</el-form-item>
+			<el-form-item label="是否有效" prop="status">
+				<el-switch v-model="form.status" :active-value="1" :inactive-value="0"></el-switch>
+			</el-form-item>
+			<el-form-item label="备注" prop="remark">
+				<el-input v-model="form.remark" clearable type="textarea"></el-input>
+			</el-form-item>
+		</el-form>
+		<template #footer>
+			<el-button @click="visible=false" >取 消</el-button>
+			<el-button v-if="mode!='show'" type="primary" :loading="isSaving" @click="submit()">保 存</el-button>
+		</template>
+	</el-dialog>
+</template>
+
+<script>
+	export default {
+		emits: ['success', 'closed'],
+		data() {
+			return {
+				mode: "add",
+				titleMap: {
+					add: '新增',
+					edit: '编辑',
+					show: '查看'
+				},
+				visible: false,
+				isSaving: false,
+				//表单数据
+				form: {
+					id:"",
+					name: "",
+					code: "",
+					label: "",
+					sort: null,
+					isGlobal: 0,
+					status: 1,
+					remark: ""
+				},
+				//验证规则
+				rules: {
+					name: [
+						{required: true, message: '请输入商品规格名称'}
+					],
+				},
+				categoryOptions: [],
+				categoryProps: {
+					value: 'id',
+					label: 'name',
+					multiple: true,
+					checkStrictly: true,
+					emitPath: false,
+					expandTrigger: "hover",
+				},
+				globalOptions: [
+					{label: "指定分类", value: 0,},
+					{label: "全部分类", value: 1,},
+				],
+			}
+		},
+		mounted() {
+			this.getCategoryList()
+		},
+		methods: {
+			//显示
+			open(mode='add'){
+				this.mode = mode;
+				this.visible = true;
+				return this
+			},
+			async getCategoryList(){
+				var res = await this.$API.store.category.tree.get();
+				this.categoryOptions = res.data
+			},
+			//表单提交方法
+			submit(){
+				this.$refs.dialogForm.validate(async (valid) => {
+					if (valid) {
+						this.isSaving = true;
+						var res;
+						if (this.form.id) {
+							res = await this.$API.store.productSpec.update.put(this.form)
+						} else {
+							res = await this.$API.store.productSpec.add.post(this.form)
+						}
+						this.isSaving = false;
+						if(res.code == 200){
+							this.form = res.data
+							this.$emit('success', this.form, this.mode)
+							this.visible = false;
+							this.$message.success("操作成功")
+						}else{
+							await this.$alert(res.message, "提示", {type: 'error'})
+						}
+					}
+				})
+			},
+			//表单注入数据
+			async setData(data){
+				Object.assign(this.form, data)
+				if (data.id){
+					this.isSaving = true
+					let reqData = {id: data.id}
+					let res = await this.$API.store.productSpec.detail.get(reqData)
+					this.isSaving = false
+					this.form = res.data
+				}
+				// this.form.isGlobal = this.form.isGlobal || 0
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
