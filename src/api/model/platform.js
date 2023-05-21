3a70ced7a604ecd83421dddd17b4108c1549d035
鸿宇 <1527200768@qq.com>
2023-05-21 14:19:21 +0800

    v1.0.0 Initial commit

--- src/api/model/platform.js
+++ src/api/model/platform.js
@@ -699,35 +699,35 @@ export default {
 		},
 		user: {
 			list: {
-				url: `${config.API_URL}/platform/user`,
+				url: `${config.API_URL}/platform/user/user`,
 				name: "列表",
 				get: async function(data){
 					return await http.get(this.url, data);
 				}
 			},
 			add: {
-				url: `${config.API_URL}/platform/user`,
+				url: `${config.API_URL}/platform/user/user`,
 				name: "添加",
 				post: async function(data){
 					return await http.post(this.url, data);
 				}
 			},
 			detail: {
-				url: `${config.API_URL}/platform/user`,
+				url: `${config.API_URL}/platform/user/user`,
 				name: "查看",
 				get: async function(data){
 					return await http.get(`${this.url}/${data.id}`, data);
 				}
 			},
 			update: {
-				url: `${config.API_URL}/platform/user`,
+				url: `${config.API_URL}/platform/user/user`,
 				name: "更新",
 				put: async function(data){
 					return await http.put(`${this.url}`, data);
 				}
 			},
 			delete: {
-				url: `${config.API_URL}/platform/user`,
+				url: `${config.API_URL}/platform/user/user`,
 				name: "删除",
 				delete: async function(data){
 					/*let id = data.id;
