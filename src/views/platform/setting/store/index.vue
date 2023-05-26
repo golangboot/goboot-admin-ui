17620365d628c2303889fcb5068fb0f9ed4b47ac
鸿宇 <1527200768@qq.com>
2023-05-27 07:02:46 +0800

    v1.0.0 Initial commit

--- src/views/platform/setting/store/index.vue
+++ src/views/platform/setting/store/index.vue
@@ -129,42 +129,131 @@
           </el-tab-pane>
 
           <el-tab-pane label="订单设置">
-            <el-form-item label-width="200px" label="订单自动关闭时间(分钟)" prop="order.autoCloseOrderTimer">
+            <el-form-item label-width="180px" label="订单自动关闭时间(分钟)" prop="order.autoCloseOrderTimer">
               <el-input-number v-model="form['store.order.autoCloseOrderTimer']" controls-position="right"></el-input-number>
               <div class="el-form-item-msg">订单提交后待支付时长</div>
             </el-form-item>
-            <el-form-item label-width="200px" label="订单自动收货时间(天)" prop="order.autoTakeOrderTimer">
+            <el-form-item label-width="180px" label="订单自动收货时间(天)" prop="order.autoTakeOrderTimer">
               <el-input-number v-model="form['store.order.autoTakeOrderTimer']" controls-position="right"></el-input-number>
               <div class="el-form-item-msg">订单自动收货时间(天) 自发货日算起；当设置为0时，默认自动收货时间是15天。</div>
             </el-form-item>
-            <el-form-item label-width="200px" label="订单售后时长(天)" prop="order.orderRefundTimer">
+            <el-form-item label-width="180px" label="订单售后时长(天)" prop="order.orderRefundTimer">
               <el-input-number v-model="form['store.order.orderRefundTimer']" controls-position="right"></el-input-number>
               <div class="el-form-item-msg">例如：设置10天指用户确认收货10天内可以退货；如未设置，默认用户在确认收货后15天就不能退货了。</div>
             </el-form-item>
-            <el-form-item label-width="200px" label="自动处理退款订单期限（天）" prop="order.autoRefundTimer">
+            <el-form-item label-width="180px" label="自动处理商户退款订单期限（天）" prop="order.autoRefundTimer">
               <el-input-number v-model="form['store.order.autoRefundTimer']" controls-position="right"></el-input-number>
-              <div class="el-form-item-msg">自动处理商户退款订单期限（天） 申请退款的订单超过期限，将自动退款处理。</div>
+              <div class="el-form-item-msg">申请退款的订单超过期限，将自动退款处理。</div>
             </el-form-item>
-            <el-form-item label-width="200px" label="订单自动好评状态" prop="order.openAutoComment">
-              <el-switch
-                  v-model="form['store.order.openAutoComment']"
-                  class="form-switch"
-                  inline-prompt
-                  style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
-                  :active-value="'1'" :inactive-value="'0'"
-                  active-text="开启" inactive-text="关闭"
-              />
-              <div class="el-form-item-msg">开启后，会自动对n天内未评价的订单默认五星好评</div>
-            </el-form-item>
-            <el-form-item label-width="200px" label="订单自动评价时间(天)" prop="order.autoCommentTimer">
-              <el-input-number v-model="form['store.order.autoCommentTimer']" controls-position="right"></el-input-number>
-              <div class="el-form-item-msg">订单自动好评触发时间</div>
-            </el-form-item>
-
             <el-row :gutter="20">
               <el-col :span="12">
+                <el-form-item label-width="180px" label="订单自动好评状态" prop="order.openAutoComment">
+                  <el-switch
+                      v-model="form['store.order.openAutoComment']"
+                      class="form-switch"
+                      inline-prompt
+                      style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+                      :active-value="'1'" :inactive-value="'0'"
+                      active-text="开启" inactive-text="关闭"
+                  />
+                  <div class="el-form-item-msg">开启后，会自动对n天内未评价的订单默认五星好评</div>
+                </el-form-item>
               </el-col>
               <el-col :span="12">
+                <el-form-item label-width="180px" label="订单自动好评触发时间(天)" prop="order.autoCommentTimer">
+                  <el-input-number v-model="form['store.order.autoCommentTimer']" controls-position="right"></el-input-number>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-form-item label-width="180px" label="订单退款理由" prop="order.orderRefundReason">
+              <el-input v-model="form['store.order.orderRefundReason']" :autosize="{ minRows: 6, maxRows: 12 }" :maxlength="65535" :show-word-limit="true" type="textarea"></el-input>
+              <div class="el-form-item-msg">每行表示一个退款理由选项，多个退款理由使用Enter确认键换行</div>
+            </el-form-item>
+
+          </el-tab-pane>
+
+          <el-tab-pane label="商家设置">
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <el-form-item label="开启商家入驻" prop="merchant.enabled">
+                  <template #label="{ label }">
+                    <span>{{ label }}</span>
+                    <span>
+                      <el-tooltip>
+                        <template #content>关闭后前端将隐藏商家相关界面</template>
+                        <el-icon style="vertical-align: middle;margin-top: -3px;margin-left: 3px;"><el-icon-question-filled /></el-icon>
+                      </el-tooltip>
+                    </span>
+                  </template>
+                  <el-switch
+                      v-model="form['store.merchant.enabled']"
+                      class="form-switch"
+                      inline-prompt
+                      style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+                      :active-value="'1'" :inactive-value="'0'"
+                      active-text="开启" inactive-text="关闭"
+                  />
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <el-form-item label="商家资质是否必传" prop="merchant.certificationRequired">
+                  <template #label="{ label }">
+                    <span>{{ label }}</span>
+                    <span>
+											<el-tooltip>
+												<template #content>商家资质是否必传</template>
+												<el-icon style="vertical-align: middle;margin-top: -3px;margin-left: 3px;"><el-icon-question-filled /></el-icon>
+											</el-tooltip>
+										</span>
+                  </template>
+                  <el-switch
+                      v-model="form['store.merchant.certificationRequired']"
+                      class="form-switch"
+                      inline-prompt
+                      style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+                      :active-value="'1'" :inactive-value="'0'"
+                      active-text="开启" inactive-text="关闭"
+                  />
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <el-form-item label="店铺保证金图标显示状态" prop="merchant.depositIconShowStatus">
+                  <template #label="{ label }">
+                    <span>{{ label }}</span>
+                    <span>
+											<el-tooltip>
+												<template #content>店铺保证金图标显示状态</template>
+												<el-icon style="vertical-align: middle;margin-top: -3px;margin-left: 3px;"><el-icon-question-filled /></el-icon>
+											</el-tooltip>
+										</span>
+                  </template>
+                  <el-switch
+                      v-model="form['store.merchant.depositIconShowStatus']"
+                      class="form-switch"
+                      inline-prompt
+                      style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+                      :active-value="'1'" :inactive-value="'0'"
+                      active-text="显示" inactive-text="隐藏"
+                  />
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="保证金图标" prop="merchant.depositIcon" clearable>
+                  <template #label="{ label }">
+                    <span>{{ label }}</span>
+                    <span>
+											<el-tooltip>
+												<template #content>店铺保证金图标</template>
+												<el-icon style="vertical-align: middle;margin-top: -3px;margin-left: 3px;"><el-icon-question-filled /></el-icon>
+											</el-tooltip>
+										</span>
+                  </template>
+                  <sc-upload :width="80" :height="80" v-model="form['store.merchant.depositIcon']" title="请上传图片"></sc-upload>
+                </el-form-item>
               </el-col>
             </el-row>
           </el-tab-pane>
