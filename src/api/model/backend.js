85f8e991dd580343944173165810bbabc086a3e3
鸿宇 <1527200768@qq.com>
2023-06-08 17:37:30 +0800

    v1.0.0 Initial commit

--- src/api/model/backend.js
+++ src/api/model/backend.js
@@ -2165,5 +2165,43 @@ export default {
 				}
 			},
 		},
+		friendlyLink: {
+			list: {
+				url: `${config.API_URL}/backend/store/friendlyLink`,
+				name: "列表",
+				get: async function(data){
+					return await http.get(this.url, data);
+				}
+			},
+			add: {
+				url: `${config.API_URL}/backend/store/friendlyLink`,
+				name: "添加",
+				post: async function(data){
+					return await http.post(this.url, data);
+				}
+			},
+			detail: {
+				url: `${config.API_URL}/backend/store/friendlyLink`,
+				name: "查看",
+				get: async function(data){
+					return await http.get(`${this.url}/${data.id}`, data);
+				}
+			},
+			update: {
+				url: `${config.API_URL}/backend/store/friendlyLink`,
+				name: "更新",
+				put: async function(data){
+					return await http.put(`${this.url}`, data);
+				}
+			},
+			delete: {
+				url: `${config.API_URL}/backend/store/friendlyLink`,
+				name: "删除",
+				delete: async function(data){
+					let id = (Array.isArray(data.ids) ? data.ids.filter(Boolean) : []).concat(data.id || []).filter(Boolean).map(String).join(',') || '';
+					return await http.delete(`${this.url}/${id}`, data);
+				}
+			},
+		},
 	},
 }
