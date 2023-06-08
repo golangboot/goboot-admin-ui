5d4e83883af185ac74f5ff932cf4b58c96cd2452
鸿宇 <1527200768@qq.com>
2023-06-08 17:41:35 +0800

    v1.0.0 Initial commit

--- src/api/model/site.js
+++ src/api/model/site.js
@@ -3,7 +3,7 @@ import http from "@/utils/request"
 
 export default {
 	info: {
-		url: `${config.API_URL}/site/info`,
+		url: `${config.API_URL}/app/site/info`,
 		name: "网站信息",
 		get: async function () {
 			return await http.get(this.url);
